f555a003d1e20839338c56efefec9ff926287cee
elisemyr <153023298+elisemyr@users.noreply.github.com>
2023-12-05 21:41:50 +0100

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,125 @@
-# This is a sample Python script.
+import turtle
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+screen = turtle.Screen()
+screen.setup(864, 864)
+screen.title("Tic Tac Toe @ 2 Players")
+screen.setworldcoordinates(-5, -5, 5, 5)
+screen.bgpic("gradsquare.gif")
+screen.tracer(0, 0)
+turtle.hideturtle()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def draw_board():
+    turtle.pencolor('light gray')
+    turtle.pensize(10)
+    turtle.up()
+    turtle.goto(-3, -1)
+    turtle.seth(0)
+    turtle.down()
+    turtle.forward(6)
+    turtle.up()
+    turtle.goto(-3, 1)
+    turtle.seth(0)
+    turtle.down()
+    turtle.fd(6)
+    turtle.up()
+    turtle.goto(-1, -3)
+    turtle.seth(90)
+    turtle.down()
+    turtle.fd(6)
+    turtle.up()
+    turtle.goto(1, -3)
+    turtle.seth(90)
+    turtle.down()
+    turtle.fd(6)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def draw_circle(x, y):
+    turtle.up()
+    turtle.goto(x, y - 0.75)
+    turtle.seth(0)
+    turtle.pencolor('magenta')
+    turtle.down()
+    turtle.circle(0.75, steps=100)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def draw_x(x, y):
+    turtle.color('light blue')
+    turtle.up()
+    turtle.goto(x - 0.75, y - 0.75)
+    turtle.down()
+    turtle.goto(x + 0.75, y + 0.75)
+    turtle.up()
+    turtle.goto(x - 0.75, y + 0.75)
+    turtle.down()
+    turtle.goto(x + 0.75, y - 0.75)
+
+
+def draw_piece(i, j, p):
+    if p == 0: return
+    x, y = 2 * (j - 1), -2 * (i - 1)
+    if p == 1:
+        draw_x(x, y)
+    else:
+        draw_circle(x, y)
+
+
+def draw(b):
+    draw_board()
+    for i in range(3):
+        for j in range(3):
+            draw_piece(i, j, b[i][j])
+    screen.update()
+
+
+# return 1 if player 1 wins, 2 if player 2 wins, 3 if tie, 0 if game is not over
+def gameover(b):
+    if b[0][0] > 0 and b[0][0] == b[0][1] and b[0][1] == b[0][2]: return b[0][0]
+    if b[1][0] > 0 and b[1][0] == b[1][1] and b[1][1] == b[1][2]: return b[1][0]
+    if b[2][0] > 0 and b[2][0] == b[2][1] and b[2][1] == b[2][2]: return b[2][0]
+    if b[0][0] > 0 and b[0][0] == b[1][0] and b[1][0] == b[2][0]: return b[0][0]
+    if b[0][1] > 0 and b[0][1] == b[1][1] and b[1][1] == b[2][1]: return b[0][1]
+    if b[0][2] > 0 and b[0][2] == b[1][2] and b[1][2] == b[2][2]: return b[0][2]
+    if b[0][0] > 0 and b[0][0] == b[1][1] and b[1][1] == b[2][2]: return b[0][0]
+    if b[2][0] > 0 and b[2][0] == b[1][1] and b[1][1] == b[0][2]: return b[2][0]
+    p = 0
+    for i in range(3):
+        for j in range(3):
+            p += (1 if b[i][j] > 0 else 0)
+    if p == 9:
+        return 3
+    else:
+        return 0
+
+def display_message(message):  
+    # Display the message in the center of the new window
+    turtle.penup()
+    turtle.goto(0, 0)
+    turtle.pendown()
+    turtle.color('black')
+    turtle.write(message, align="center", font=("Courier New", 50, "normal"))
+
+def play(x, y):
+    global turn
+    i = 3 - int(y + 5) // 2
+    j = int(x + 5) // 2 - 1
+    if i > 2 or j > 2 or i < 0 or j < 0 or b[i][j] != 0: return
+    if turn == 'x':
+        b[i][j], turn = 1, 'o'
+    else:
+        b[i][j], turn = 2, 'x'
+    draw(b)
+    r = gameover(b)
+    if r == 1:
+        screen.textinput("Game over!", "X won!")
+    elif r == 2:
+        screen.textinput("Game over!", "O won!")
+    elif r == 3:
+        screen.textinput("Game over!", "Tie!")
+
+
+b = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+draw(b)
+turn = 'x'
+screen.onclick(play)
+turtle.mainloop()
